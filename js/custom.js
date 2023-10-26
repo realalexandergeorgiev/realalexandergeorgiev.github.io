4a087419344daa426cede2edca9ef9ec5f9c82fb
alex <alex@localhost>
2023-10-26 16:29:01 +0200

    updated main page

--- js/custom.js
+++ js/custom.js
@@ -4,3 +4,40 @@ $('#siteNav').affix({
 		top: 100
 	}
 })
+
+$('.slick').slick({
+	infinite: true,
+	slidesToShow: 4,
+	slidesToScroll: 1,
+	autoplay: true,
+  	autoplaySpeed: 2000,
+	
+
+	responsive: [
+		{
+			breakpoint: 1024,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 1,
+				infinite: true,
+				dots: true
+			}
+		},
+		{
+			breakpoint: 600,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 1
+			}
+		},
+		{
+			breakpoint: 480,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
+			}
+		}
+
+	]
+});
+
