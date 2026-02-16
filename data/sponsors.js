895ccf182e6441fce1397a12583c8432ea42365e
realalexandergeorgiev <alexander.georgiev@daloo.de>
2026-02-16 09:39:36 +0100

    feat: Add `forceColor` property to sponsor data and conditionally apply a color filter to sponsor logos based on this property.

--- data/sponsors.js
+++ data/sponsors.js
@@ -7,27 +7,32 @@ Object.assign(window.BSidesData, {
             {
                 name: "BSidesMunich",
                 logo: "images/sponsors/BSidesMunich.svg.png",
-                url: "https://bsidesmunich.org/"
+                url: "https://bsidesmunich.org/",
+                forceColor: true
             },
             {
                 name: "BSidesBerlin",
                 logo: "images/sponsors/bsidesberlin.png",
-                url: "https://bsidesberlin.org/"
+                url: "https://bsidesberlin.org/",
+                forceColor: true
             },
             {
                 name: "Elbsides",
                 logo: "images/sponsors/Elbsides_Logo_2026.svg",
-                url: "https://elbsides.de/"
+                url: "https://elbsides.de/",
+                forceColor: false
             },
             {
                 name: "BSidesDresden",
                 logo: "images/sponsors/bsidesdresden.png",
-                url: "https://bsidesdresden.com/"
+                url: "https://bsidesdresden.com/",
+                forceColor: true
             },
             {
                 name: "BSidesTransylvania",
                 logo: "images/sponsors/bsidestransylvania.png",
-                url: "https://bsidestransylvania.com/"
+                url: "https://bsidestransylvania.com/",
+                forceColor: true
             }
         ]
     }
