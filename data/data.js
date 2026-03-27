73c517d5828bdf23f95423f9d8e1b48c4d4a12f9
realalexandergeorgiev <alexander.georgiev@daloo.de>
2026-03-27 13:51:06 +0100

    added tickets link

--- data/data.js
+++ data/data.js
@@ -16,7 +16,7 @@ Object.assign(window.BSidesData, {
             }
         },
         "links": {
-            "tickets": "#",
+            "tickets": "https://ticket.bsidesfrankfurt.org",
             "cfp": "https://cfp.bsidesfrankfurt.org",
             "ctf": "#"
         },
